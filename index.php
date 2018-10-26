0ad9af8210b72aa7184fa331561af3eb8c987fdb
daniel907 <danieldelgadomelchor@gmail.com>
2018-10-25 23:54:40 -0500

    SE MODIFICO EL TITULO DEL INDEX

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title>Bien venido a examen en línea | tusolutionweb.blogspot.pe</title>
+<title>EXAMENES DEPARTAMENTALES</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <link href="quiz.css" rel="stylesheet" type="text/css">
 
