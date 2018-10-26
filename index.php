f83e23e1264ebf006df79a080c91eb51c21ca9d1
checheclothes <joseluisespinosagarcia11660@gmail.com>
2018-10-26 00:00:52 -0500

    Se autentifico para utf-8 y escuela papi roger

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="es">
 <head>
 <title>EXAMENES DEPARTAMENTALES</title>
+    <meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <link href="quiz.css" rel="stylesheet" type="text/css">
 
